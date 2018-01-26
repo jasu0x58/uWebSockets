baf25ab431312f0dc8de82803929c2c41f29063b
Jan Sutter <jasu0x58@gmail.com>
2018-01-26 15:10:41 +0100

    fix bug in zlib ep configure command regarding build type

--- cmake/ep/zlib.cmake
+++ cmake/ep/zlib.cmake
@@ -1,10 +1,14 @@
+if(CMAKE_BUILD_TYPE MATCHES "Debug")
+    set(zlib_build_type --debug)
+endif()
+
 ExternalProject_Add(ep_zlib
     GIT_REPOSITORY https://github.com/madler/zlib
     GIT_TAG v1.2.11
     UPDATE_COMMAND ""
     INSTALL_DIR ${EP_INSTALL_PREFIX}
     BUILD_IN_SOURCE 1    
-    CONFIGURE_COMMAND ./configure $<$<CONFIG:Debug>:--debug> --static --prefix=<INSTALL_DIR>
+    CONFIGURE_COMMAND ./configure ${zlib_build_type} --static --prefix=<INSTALL_DIR>
     BUILD_COMMAND ${MAKE_COMMAND}
     INSTALL_COMMAND ${MAKE_COMMAND} install
 )
